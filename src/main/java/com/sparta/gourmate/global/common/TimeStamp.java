5db0914eb90b4e3455c822095f40770a132cb7b4
sohyuneeee <sohyuney@naver.com>
2024-11-07 15:40:21 +0900

    feat: 기본 config 설정

---
+++ src/main/java/com/sparta/gourmate/global/common/TimeStamp.java
@@ -0,0 +1,13 @@
+package com.sparta.gourmate.global.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimeStamp {
+
+}
