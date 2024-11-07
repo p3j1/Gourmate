a4786c1d66d7c3167e4d9817f0406c5b42554dc3
sohyuneeee <sohyuney@naver.com>
2024-11-07 15:19:57 +0900

    init commit

---
+++ src/main/java/com/sparta/gourmate/GourmateApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.gourmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GourmateApplication.class, args);
+    }
+
+}
