9dc142fbb0f1f3b74840819d28c3b4cbc6a5a4b6
sohyuneeee <sohyuney@naver.com>
2024-11-09 14:05:09 +0900

    refactor #5: MenuStatusEnum 클래스 코드 리팩토링

--- src/main/java/com/sparta/gourmate/domain/menu/entity/MenuStatusEnum.java
+++ src/main/java/com/sparta/gourmate/domain/menu/entity/MenuStatusEnum.java
@@ -1,24 +1,15 @@
 package com.sparta.gourmate.domain.menu.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MenuStatusEnum {
-    AVAILABLE(status.AVAILABLE),
-    OUT_OF_STOCK(status.OUT_OF_STOCK),
-    HIDDEN(status.HIDDEN);
+    AVAILABLE("AVAILABLE"),
+    OUT_OF_STOCK("OUT_OF_STOCK"),
+    HIDDEN("HIDDEN");
 
     private final String MenuStatus;
-
-    MenuStatusEnum(String menuStatus) {
-        this.MenuStatus = menuStatus;
-    }
-
-    public String getMenuStatus() {
-        return this.MenuStatus;
-    }
-
-    public static class status {
-        public static final String AVAILABLE = "AVAILABLE";
-        public static final String OUT_OF_STOCK = "OUT_OF_STOCK";
-        public static final String HIDDEN = "HIDDEN";
-    }
 
 }
