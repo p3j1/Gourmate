fd3ffe63c20e4e744ad1b1f74d0472e5452fba00
jelliclesu <jelliclesu@naver.com>
2024-11-14 14:57:37 +0900

    fix #7: unique 조건 추가

--- src/main/java/com/sparta/gourmate/domain/store/entity/Category.java
+++ src/main/java/com/sparta/gourmate/domain/store/entity/Category.java
@@ -21,7 +21,7 @@ public class Category extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @OneToMany(mappedBy = "category")
--- src/main/java/com/sparta/gourmate/domain/store/entity/Store.java
+++ src/main/java/com/sparta/gourmate/domain/store/entity/Store.java
@@ -24,7 +24,7 @@ public class Store extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
