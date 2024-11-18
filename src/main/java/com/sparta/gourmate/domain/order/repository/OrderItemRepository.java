569eaa0e6f989c8853f198ed61185ee9b2bd7e85
yjm07 <jyeon9506@gmail.com>
2024-11-18 10:04:39 +0900

    fix: Long -> UUID

--- src/main/java/com/sparta/gourmate/domain/order/repository/OrderItemRepository.java
+++ src/main/java/com/sparta/gourmate/domain/order/repository/OrderItemRepository.java
@@ -4,7 +4,9 @@ import com.sparta.gourmate.domain.order.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, UUID> {
 
 }
